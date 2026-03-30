389680f0258bc57eaf2aec60d68a6d2284d501fb
Thiago de Mello Bueno <thiago@madeofpeople.org>
2026-03-30 23:21:52 +0000

    enqueue assets, adds taxonomies

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,11 +2,34 @@
 
 namespace abcnorio\CustomFunc;
 
+use abcnorio\CustomFunc\ContentModel\PostTypeRegistrar;
+use abcnorio\CustomFunc\ContentModel\TaxonomyRegistrar;
+
 final class Plugin
 {
     public static function activate(): void
     {
         self::registerContentModels();
+    }
+
+    public static function enqueueEditorAssets(): void
+    {
+        $buildDir = plugin_dir_path(ABCNORIO_CUSTOM_FUNC_FILE) . 'build/';
+        $assetFile = $buildDir . 'index.asset.php';
+
+        if (! file_exists($assetFile)) {
+            return;
+        }
+
+        $asset = require $assetFile;
+
+        wp_enqueue_script(
+            'abcnorio-custom-func-editor',
+            plugin_dir_url(ABCNORIO_CUSTOM_FUNC_FILE) . 'build/index.js',
+            $asset['dependencies'],
+            $asset['version'],
+            true
+        );
     }
 
     public static function boot(): void
@@ -18,6 +41,8 @@ final class Plugin
     public static function registerContentModels(): void
     {
         $postTypes = require __DIR__ . '/ContentModel/post-types.php';
+        $taxonomies = require __DIR__ . '/ContentModel/taxonomies.php';
         PostTypeRegistrar::registerMany($postTypes);
+        TaxonomyRegistrar::registerMany($taxonomies);
     }
 }
